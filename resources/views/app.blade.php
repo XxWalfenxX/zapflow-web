7dab13b87cb24949642d8c2c5fa64428ffaed8af
Walfen <xxwalfenxx@gmail.com>
2023-10-10 14:01:43 +0200

    favicon and SEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
+        <meta name="title" content="ZapFLow" />
+        <meta name="description" content="Tu red de carga de confianza">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="shortcut icon" type="image/png" href="{{ asset('/img/favicon/android-chrome-192x192.png') }}">
+        <link rel="shortcut icon" sizes="192x192" href="{{ asset('/img/favicon/android-chrome-192x192.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
