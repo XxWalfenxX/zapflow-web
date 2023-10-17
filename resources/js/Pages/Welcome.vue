c01ff0fb82aba776d7ad5fb6f6eed0edc64524d7
Walfen <xxwalfenxx@gmail.com>
2023-10-17 15:49:28 +0200

    map

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,9 +2,8 @@
 import { Head } from "@inertiajs/vue3";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import GuestMap from "@/Components/GuestMap.vue";
 import { ref } from "vue";
-import axios from "axios";
 
 defineProps({
     canLogin: {
@@ -15,23 +14,14 @@ defineProps({
     },
 });
 
-var puntoCargaMapa = ref();
 
-axios.get("/api/map").then((response) => {
-    puntoCargaMapa = response.data
-});
-
-let zoom = ref(12);
-let center = ref([41.3732, 2.154]);
-
-var mapURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     mapURL =
         "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
 }
 
-var currentEstacion = ref("");
+
 </script>
 
 <template>
@@ -41,58 +31,7 @@ var currentEstacion = ref("");
         :canRegister="canRegister"
         :propUser="$page.props.auth.user"
     >
-        <div
-            class="flex flex-col md:flex-row gap-4 m-8 h-[80vh] text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700"
-        >
-            <div
-                class="w-full md:w-1/4 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700"
-            >
-                <h5
-                    v-if="currentEstacion == ''"
-                    class="mb-2 text-3xl font-bold text-gray-900 dark:text-white"
-                >
-                    Elige un punto de carga
-                </h5>
-                <h5
-                    v-else
-                    class="mb-2 text-3xl font-bold text-gray-900 dark:text-white"
-                >
-                    {{ currentEstacion.ubicacion }}
-                </h5>
-                <p
-                    v-if="currentEstacion == ''"
-                    class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400"
-                >
-                    Selecciona un punto de carga para ver su información actual
-                </p>
-                <p
-                    v-else
-                    class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400"
-                >
-                    {{ currentEstacion.potencia }}
-                </p>
-            </div>
-
-            <div class="flex-[1_1_50vh] md:w-3/4">
-                <l-map
-                    ref="map"
-                    v-model:zoom="zoom"
-                    v-model:center="center"
-                    :useGlobalLeaflet="false"
-                >
-                    <l-tile-layer
-                        :url="mapURL"
-                        layer-type="base"
-                        name="Stadia Maps Basemap"
-                    ></l-tile-layer>
-                    <l-marker
-                        v-for="punto in puntoCargaMapa"
-                        :lat-lng="[punto.latitud,punto.longitud]"
-                        @click="currentEstacion = punto"
-                    ></l-marker>
-                </l-map>
-            </div>
-        </div>
+        <GuestMap></GuestMap>
     </GuestLayout>
 </template>
 
