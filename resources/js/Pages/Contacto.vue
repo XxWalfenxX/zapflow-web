da7636b98998d7e41a7c9f076df764f77c111714
Walfen <xxwalfenxx@gmail.com>
2023-10-31 19:15:33 +0100

    Update Contacto.vue

--- resources/js/Pages/Contacto.vue
+++ resources/js/Pages/Contacto.vue
@@ -111,32 +111,12 @@ defineProps({
                             >Número de telefono</label
                         >
                         <div class="relative mt-2.5">
-                            <div
-                                class="absolute inset-y-0 left-0 flex items-center"
-                            >
-                                <label for="country" class="sr-only"
-                                    >Country</label
-                                >
-                                <select
-                                    id="country"
-                                    name="country"
-                                    class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                                >
-                                    <option>US</option>
-                                    <option>CA</option>
-                                    <option>EU</option>
-                                </select>
-                                <ChevronDownIcon
-                                    class="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                                    aria-hidden="true"
-                                />
-                            </div>
                             <input
                                 type="tel"
                                 name="phone-number"
                                 id="phone-number"
                                 autocomplete="tel"
-                                class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -144,7 +124,7 @@ defineProps({
                         <label
                             for="message"
                             class="block text-sm font-semibold leading-6 text-gray-900"
-                            >Message</label
+                            >Mensaje</label
                         >
                         <div class="mt-2.5">
                             <textarea
@@ -164,7 +144,7 @@ defineProps({
                                     'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
                                 ]"
                             >
-                                <span class="sr-only">Agree to policies</span>
+                                <span class="sr-only">Aceptar las politicas</span>
                                 <span
                                     aria-hidden="true"
                                     :class="[
@@ -177,10 +157,10 @@ defineProps({
                             </Switch>
                         </div>
                         <SwitchLabel class="text-sm leading-6 text-gray-600">
-                            By selecting this, you agree to our
+                            Al seleccionar esto, usted acepta nuestra
                             {{ " " }}
-                            <a href="#" class="font-semibold text-indigo-600"
-                                >privacy&nbsp;policy</a
+                            <a :href="route('privacy')" class="font-semibold text-indigo-600"
+                                >política de privacidad</a
                             >.
                         </SwitchLabel>
                     </SwitchGroup>
