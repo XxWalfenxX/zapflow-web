e5113d07057b6d8c7eada8f5de5eac70ab19d80a
Walfen <xxwalfenxx@gmail.com>
2023-12-05 02:37:54 +0100

    fix datatable

--- resources/js/Pages/Admin/AdminUsers.vue
+++ resources/js/Pages/Admin/AdminUsers.vue
@@ -4,9 +4,9 @@ import { Head } from '@inertiajs/vue3';
 import AdminDashboard from '../../Layouts/AdminDashboard.vue';
 import DataTable from 'datatables.net-vue3';
 import DataTablesCore from 'datatables.net';
+import 'datatables.net-responsive';
 
 DataTable.use(DataTablesCore);
-
 
 defineProps({
     users: {
@@ -15,10 +15,12 @@ defineProps({
 
 });
 
-const data = [
-    [1, 2],
-    [3, 4],
-];
+const options = {
+    responsive: true,
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json',
+    },
+};
 
 </script>
 
@@ -28,66 +30,75 @@ const data = [
     <AdminDashboard>
         <h1 class="font-bold text-4xl mb-3">Gestionar Usuarios</h1>
 
-        <DataTable :data="data" class="display">
-            <thead>
-                <tr>
-                    <th>A</th>
-                    <th>B</th>
-                </tr>
-            </thead>
-        </DataTable>
 
-
+        <div class="mb-3">
+            <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="w-3.5 h-3.5 me-2 text-white" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 1v16M1 9h16" />
+                </svg>
+                Añadir Usuario
+            </button>
+        </div>
         <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <DataTable :options="options" class="display w-full">
+                <thead>
                     <tr>
-                        <th scope="col" class="px-6 py-3">
-                            id
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Nombre
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            email
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Contraseña
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Fecha de creación
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Fecha de actualización
-                        </th>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Correo Electrónico</th>
+                        <th>Contraseña</th>
+                        <th>Fecha de creación</th>
+                        <th>Fecha última actualización</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="user in users" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr v-for="user in users">
+                        <th>
                             {{ user.id }}
                         </th>
-                        <td class="px-6 py-4">
+                        <td>
                             {{ user.name }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td>
                             {{ user.email }}
                         </td>
-                        <td class="px-6 py-4">
-                            {{ user.password }}
+                        <td>
+                            {{ user.password.length > 15 ? user.password.slice(0, 15) + "..." : user.password }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td>
                             {{ user.created_at }}
                         </td>
-                        <td class="px-6 py-4">
+                        <td>
                             {{ user.updated_at !== null ? user.updated_at : 'No hay datos' }}
+                        </td>
+                        <td>
+                            <button type="button"
+                                class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">
+                                <svg class="w-6 h-6 text-white" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                    <path
+                                        d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z" />
+                                    <path
+                                        d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z" />
+                                </svg>
+                            </button>
+                            <button type="button"
+                                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                <svg class="w-6 h-6 text-white" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path
+                                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                                </svg></button>
                         </td>
                     </tr>
                 </tbody>
-            </table>
+            </DataTable>
         </div>
+
     </AdminDashboard>
 </template>
-<style>
-@import 'datatables.net-dt';
-</style>
+<style></style>
