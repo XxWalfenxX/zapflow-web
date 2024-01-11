1250f93601b030e20103c928951dea5b50e1fded
firagonb3 <golenpiedra@gmail.com>
2024-01-11 18:43:35 +0100

    addbonus

--- resources/js/Components/Modal/Bonos/AddBonos.vue
+++ resources/js/Components/Modal/Bonos/AddBonos.vue
@@ -98,16 +98,25 @@ const addDesc = () => {
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripciones</label>
                             <button @click.prevent="addDesc"
                                 class="mb-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
                                 Agregar descripción
                             </button>
 
                             <!-- Mostrar inputs adicionales -->
-                            <div v-for="(description, index) in additionalDescriptions" :key="index">
-                                <input type="text" :name="'des' + index" :id="'des' + index"
-                                    v-model="additionalDescriptions[index]"
-                                    class="bg-gray-50 border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    required>
+                            <div class=" h-80 overflow-y-auto">
+                                <div v-for="(description, index) in additionalDescriptions" :key="index">
+                                    <input type="text" :name="'des' + index" :id="'des' + index"
+                                        v-model="additionalDescriptions[index]"
+                                        class="bg-gray-50 border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        required>
+                                </div>
                             </div>
+
 
                         </div>
 
